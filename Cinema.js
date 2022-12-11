f3c10dde638b270eead36d317918eaf99af82d3d
stassold <Stason-Kobzon@mail.ru>
2022-12-11 15:10:06 +0700

    Modyfided id kinopoisk

--- Cinema.js
+++ Cinema.js
@@ -1,9 +1,15 @@
-let Find = () => 
-{
-    let id = document.getElementById("Id_cinema").value
-    console.log(id)
-    let Now_film  = document.getElementById("yohoho").dataset.kinopoisk;
-    console.log(Now_film)
-    Now_film = id;
-    console.log(Now_film)
+let elem = document.createElement("div")
+let elemText = document.createTextNode("Кино")
+elem.id = "yohoho"
+elem.appendChild(elemText)
+
+let Find = () => {
+    let find_input = document.getElementById("Id_cinema")
+    if (find_input.value) {
+        let id = find_input.value
+        elem.dataset.kinopoisk = id
+        document.body.appendChild(elem)
+        find_input.value = null
+    }
+
 }
