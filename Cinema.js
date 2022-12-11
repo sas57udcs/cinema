f070d4370557930b129f9571a345af49abde061e
stassold <Stason-Kobzon@mail.ru>
2022-12-11 13:50:00 +0700

    Starting

---
+++ Cinema.js
@@ -0,0 +1,9 @@
+let Find = () => 
+{
+    let id = document.getElementById("Id_cinema").value
+    console.log(id)
+    let Now_film  = document.getElementById("yohoho").dataset.kinopoisk;
+    console.log(Now_film)
+    Now_film = id;
+    console.log(Now_film)
+}
