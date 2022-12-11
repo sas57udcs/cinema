e78f99e321f4e5c5b3c2bf7886078ef8cd72958a
stassold <Stason-Kobzon@mail.ru>
2022-12-11 17:30:18 +0700

    hash router

--- Cinema.js
+++ Cinema.js
@@ -2,19 +2,27 @@ let elem = document.createElement("div")
 let elemText = document.createTextNode("Кино")
 elem.id = "yohoho"
 elem.appendChild(elemText)
+//let baseurl = new URL('https://sas57udcs.github.io/cinema/')
+let baseurl = new URL('file:///C:/Users/ssa/Downloads/Cinema/index.html')
+let hash_id = window.location.hash
 
-let elemscript = document.createElement('script');
-elemscript.type = 'text/javascript';
-elemscript.src = '//yohoho.cc/yo.js';
+let loading_player = () => {
+    if (hash_id) {
+        let id = hash_id.substring(1)
+        elem.dataset.kinopoisk = id
+        document.body.appendChild(elem)
+        find_input.value = null
+    }
+}
 
 let Find = () => {
     let find_input = document.getElementById("Id_cinema")
     if (find_input.value) {
         let id = find_input.value
-        elem.dataset.kinopoisk = id
-        document.body.appendChild(elem)
-        find_input.value = null
-        document.body.appendChild(elemscript)
+        let url = new URL('#' + id, baseurl)
+        location.href = url;
+        window.location.reload();
     }
-
 }
+loading_player()
+
